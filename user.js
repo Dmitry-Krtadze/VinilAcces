6cb2ead955b9465939b26a7c7ea0fa84e9c4de12
Dmitry.Kirtdze <mr.dima.kirtadze@gmail.com>
2023-12-06 11:43:35 +0200

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	new file:   user.js 	new file:   userPage.html

---
+++ user.js
@@ -0,0 +1,21 @@
+const urlParams = URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+let profile = document.getElementById('profile');
+let url = 'https://my-json-server.typicode.com/Dmitry-Krtadze/Vinil';
+
+let urlRequest = new XMLHttpRequest();
+
+userRequest = new XMLHttpRequest();
+userRequest.open('GET',`${url}/users/${id}`);
+userRequest.responseType = 'json';
+userRequest.onload = function(){
+    let user = userRequest.response;
+    profile.innerHTML = `
+        <h1>${user.name}</h1>
+        <h2>${user.sirname}</h2>
+        <img class="profile-img" src="${user.photo_url}">
+        <p>Balanse: ${user.balance}</p> 
+    `
+}
+userRequest.send();
