6361f10dabd8569a5c88feaf36205f63228886be
Dmitry-Kirtadze <mr.dima.kirtadze@gmail.com>
2023-12-20 11:04:58 +0200

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   user.js 	modified:   userPage.html

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let productsGrid = document.getElementById('products-grid');
-let productsAray = [];
+let productsAray =[];
 let xhr = new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/Dmitry-Krtadze/Vinil';
 
 xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
+
 xhr.onload = function(){
     let products = xhr.response;
+    productsAray = xhr.response;
     productsGrid.innerHTML = null;
     products.forEach(p => {
         productsAray.push(p);
@@ -18,9 +20,50 @@ xhr.onload = function(){
             <p class='product-price'><b>Price: </b>${p.price}</p>
             <p class='product-price'><b>Description: </b>${p.description}</p>
             <a href='userPage.html?id=${p.author_id}'>Seller profile</a>
-            <button>Buy</button>
+            <button onclick="addProductToCart(${p.id});">Buy</button>
         `;
         productsGrid.append(pElem);
     });
 }
 xhr.send();
+
+let cardProd = document.getElementById('cart-products');
+let cart = [];
+
+function addProductToCart(id){
+    let product = productsAray.find(function(p){
+        return p.id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+}
+
+function drawCartProducts(){
+    if(cart.length === 0) return cardProd.innerHTML = 'Cart is empty';
+    cardProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cardProd.innerHTML += `
+            <p><img src="${p.photo_url}">
+                ${p.name} | ${p.price} $
+            </p>
+            <hr>
+
+        `;
+        sum += p.price;
+    });
+    cardProd.innerHTML += `
+        <b>Total Price: ${sum} $</b>
+        <button onclick="buyAll();">Buy All</button>
+    `;
+}
+
+function buyAll(){
+    cart = [];
+    cardProd.innerHTML = 'Money was withdrawn from your credit card';
+}
+
+function openCart(){
+    cardProd.classList.toggle('hide');
+}
+
--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-const urlParams = URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 let profile = document.getElementById('profile');
@@ -19,3 +19,41 @@ userRequest.onload = function(){
     `
 }
 userRequest.send();
+
+
+
+
+let productsGrid = document.getElementById('user-products-grid');
+let productRequest = new XMLHttpRequest();
+
+productRequest.open('GET', `${url}/products?author_id=${id}`);
+productRequest.responseType = 'json';
+productRequest.onload = function(){
+    let products = productRequest.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsGrid.innerHTML += `
+            <div class="product">
+                <h2 class='product-name'>${p.name}</h2>
+                <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
+                <p class='pruduct-price'>
+                    <b>Price: </b>${p.price}
+                </p>
+                <p class=''product-description>
+                    <b>Description: </b> ${p.description}
+                </p>
+            
+            </div>
+
+        
+        
+        `;
+    });
+}
+productRequest.send();
+
+
+
+
+
+
